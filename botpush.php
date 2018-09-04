47d75407b7ef2f1f194d4ec2e0e4fe6a3c3ed4d2
Srikieat <42906808+Srikieat@users.noreply.github.com>
2018-09-04 12:02:41 +0700

    dsfa

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $pushID = 'U44e90a4578cb725ccc9ed09d2cdc18e9';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world123');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = '0jFIiIq0JnX9WLpNo+ZMNnVKOSP3IYtDwwqLNSwnR3PyIqo+pTSIdJyY0fLkxQEBSGB7h1OA/ZlRTeHiYeb6v/B7Xnla6B2RO0oIjXfuLFKLKp5kwGc1ZwyR/Ye2KAAnD+fXr3MR7/eCN6ilzs6CQAdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$userId = 'U44e90a4578cb725ccc9ed09d2cdc18e9';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
