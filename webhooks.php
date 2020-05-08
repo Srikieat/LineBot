b51db67c07b26a027d1bfceb08ed874987297344
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-08 22:52:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -72,7 +72,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => "i am a ".[$messages],
+				'messages' => ["i am dd".$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
