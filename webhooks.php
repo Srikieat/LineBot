f07039538230140570ac7b1fc2862f1188ec1a3b
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-10 10:10:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -95,22 +95,22 @@ if (!is_null($events['events'])) {
                 'template' => array(
                     		'type' => 'buttons', // 類型 (按鈕)
 		                'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
-                 		'title' => 'Example Menu', // 標題 <不一定需要>
-		                'text' => 'Please select', // 文字
+                 		'title' => 'บ.โอเคพลัส จำกัด', // 標題 <不一定需要>
+		                'text' => 'ของ่าย ได้ไว ยอดกู้เยอะ', // 文字
                 		'actions' => array(
-			                        array(
-                            				'type' => 'postback', // 類型 (回傳)
-				                        'label' => 'Postback example', // 標籤 1
-				                        'data' => 'action=buy&itemid=123' // 資料
-                        			      ),
-			                        array(
-                            				'type' => 'message', // 類型 (訊息)
-				                        'label' => 'Message example', // 標籤 2
-				                        'text' => 'Message example' // 用戶發送文字
-				                      ),
+			                      //  array(
+                            			//	'type' => 'postback', // 類型 (回傳)
+				                 //       'label' => 'Postback example', // 標籤 1
+				                   //     'data' => 'action=buy&itemid=123' // 資料
+                        			  //    ),
+			                       // array(
+                            			//	'type' => 'message', // 類型 (訊息)
+				                 //       'label' => 'Message example', // 標籤 2
+				                   //     'text' => 'Message example' // 用戶發送文字
+				                 //     ),
 			                        array(
                         				 'type' => 'uri', // 類型 (連結)
-				                         'label' => 'Uri example', // 標籤 3
+				                         'label' => 'ลงทะเบียน', // 標籤 3
 				                         'uri' => 'http://okplus.ddns.net/okplus/OKMO/Bot.aspx?u='.$text // 連結網址
 				                       )
 			                       )
