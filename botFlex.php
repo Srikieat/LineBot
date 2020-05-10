06f56c4863e5a75d6c00896ce2098743cb618586
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-10 17:40:18 +0700

    Update botFlex.php

--- botFlex.php
+++ botFlex.php
@@ -21,7 +21,7 @@ $pushID = 'U44e90a4578cb725ccc9ed09d2cdc18e9';
 // Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'replyToken' => $replyToken,
+				'replyToken' => $access_token,
 				'messages' => [$msg],
 			];
 			$post = json_encode($data);
