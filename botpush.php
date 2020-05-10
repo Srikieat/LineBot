e42002412c808e776004c2615f6c0cc5e8edc6d2
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-10 17:22:14 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -139,7 +139,7 @@ $msg = [
 ];
 $pushID = $_GET['u'];
   //'U44e90a4578cb725ccc9ed09d2cdc18e9';
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder([$msg]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\buildMessage($msg);
 //$textMessageBuilder = $msg
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
