3ea9ff62563f3cd1b20788c8c55861f088046c87
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-11 10:35:40 +0700

    Update botFlex.php

--- botFlex.php
+++ botFlex.php
@@ -7,10 +7,157 @@
    //รับ id ของผู้ใช้
    $id = "U44e90a4578cb725ccc9ed09d2cdc18e9";
    
+//$messages = [
+				//						'type' => 'text',
+				//						'text' => 'Line25 นี้เป็นระบบอัตโนมัติ'."\n".'หากต้องการติดต่อพนักงาน โปรดติดต่อที่ https://lin.ee/6D052q8'	
+				//					];
+
+$paymentDetails = "1,430:ช่องทางการชำระเงิน Big C:14 เมษายน 2563:ชำระแล้ว 4 งวด คงค้าง 0 งวด:ราณี สายใจ:8กร 2513";
+					$str_arr = explode (":", $paymentDetails);  
+					
+					$amount=$str_arr[0];
+					$channel = $str_arr[1];
+					$dt = $str_arr[2];
+					$detail = $str_arr[3];
+					$name = $str_arr[4];
+					$plate = $str_arr[5];
+
+
 $messages = [
-										'type' => 'text',
-										'text' => 'Line25 นี้เป็นระบบอัตโนมัติ'."\n".'หากต้องการติดต่อพนักงาน โปรดติดต่อที่ https://lin.ee/6D052q8'	
-									];
+					
+					
+					
+						 "type" => "flex",
+    "altText" => "ใบเสร็จรับเงิน-ค่างวด",
+    "contents" => [
+      "type" => "bubble",
+      "direction" => "ltr",
+      "header" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "ใบเสร็จรับเงิน-ค่างวด",
+            "size" => "lg",
+            "align" => "start",
+            "weight" => "bold",
+            "color" => "#009813"
+          ],
+          [
+            "type" => "text",
+            "text" => "฿".$amount,
+            "size" => "3xl",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => $channel,
+            "size" => "lg",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => $dt,
+            "size" => "xs",
+            "color" => "#B2B2B2"
+          ],
+          [
+            "type" => "text",
+            "text" => $detail,
+            "margin" => "lg",
+            "size" => "lg",
+            "color" => "#000000"
+          ]
+        ]
+      ],
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "separator",
+            "color" => "#C3C3C3"
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "ชื่อ",
+                "align" => "start",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => $name,
+                "align" => "end",
+                "color" => "#000000"
+              ]
+            ]
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "ทะเบียนรถ",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => $plate,
+                "align" => "end"
+              ]
+            ]
+          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ]
+        ]
+      ],
+      "footer" => [
+        "type" => "box",
+        "layout" => "horizontal",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "View Details",
+            "size" => "lg",
+            "align" => "start",
+            "color" => "#0084B6",
+            "action" => [
+              "type" => "uri",
+              "label" => "View Details",
+              "uri" => "https://google.co.th/"
+            ]
+          ]
+        ]
+      ]
+    ]
+					
+					
+					
+					
+					    ];	
+
+
+
+
+
+
+
+
+
+
       
 	$data = [
 				'to' => $id,
