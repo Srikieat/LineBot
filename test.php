dd6f69b4005871ac037c769a49f2bd6d3f5b8c38
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-11 09:37:21 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,7 @@
 <?php
 
-   $ip = "123.456.789.000"; // some IP address
-   $iparr = split ("\.", $ip); 
-   
-   print "$iparr[0] <br />";
-   print "$iparr[1] <br />" ;
-   print "$iparr[2] <br />"  ;
-   print "$iparr[3] <br />"  ;
+$string = "123,46,78,000"; 
+$str_arr = explode (",", $string);  
+print_r($str_arr); 
 
 ?>
