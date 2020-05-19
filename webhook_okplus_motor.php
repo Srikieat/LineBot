21188b33145e4944cb87145283555d29b36abf0c
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-19 10:20:03 +0700

    Update webhook_okplus_motor.php

--- webhook_okplus_motor.php
+++ webhook_okplus_motor.php
@@ -87,11 +87,9 @@ if (!is_null($events['events'])) {
 
             $isHello = false;
             
-            if (strops($sendMessage,'สวัสดี') != false)
-            {
-                $isHello = true;
-            }
+           
 
+        
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
