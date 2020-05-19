49f52a1b517b559997eb2f60e0c212b5e02d46b8
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-19 10:05:38 +0700

    Update webhook_okplus_motor.php

--- webhook_okplus_motor.php
+++ webhook_okplus_motor.php
@@ -37,32 +37,7 @@ if (!is_null($events['events'])) {
 			
 			// start message
 				$messages = [
-						'type' => 'template', // 訊息類型 (模板)
-                				'altText' => 'ลงทะเบียน', // 替代文字
-                				'template' => array(
-                    						'type' => 'buttons', // 類型 (按鈕)
-		                				'thumbnailImageUrl' => 'https://okplus.co.th/Bot/Images/ImgButtonTemplate.png', // 圖片網址 <不一定需要>
-                 						'title' => 'บ.โอเคพลัส จำกัด', // 標題 <不一定需要>
-		                				'text' => 'ยินดีต้อนรับเข้าสู่ระบบ', // 文字
-                						'actions' => array(
-			                      					//  array(
-                            							//	'type' => 'postback', // 類型 (回傳)
-				                 				//       'label' => 'Postback example', // 標籤 1
-				                   				//     'data' => 'action=buy&itemid=123' // 資料
-                        			  				//    ),
-			                       					// array(
-                            							//	'type' => 'message', // 類型 (訊息)
-				                 				//       'label' => 'Message example', // 標籤 2
-				                   				//     'text' => 'Message example' // 用戶發送文字
-				                 				//     ),
-			                        				   array(
-                        				 				'type' => 'uri', // 類型 (連結)
-				                         				'label' => 'ลงทะเบียน', // 標籤 3
-				                         				'uri' => 'http://okplus.ddns.net/okplus/OKMO/Bot.aspx?u='.$text // 連結網址
-				                       				         )
-			                       					   )
-		                					)
-					
+						
 						];	
 						// end message
 			
@@ -108,50 +83,13 @@ if (!is_null($events['events'])) {
   				case "ลงทะเบียน":
    					// start message
 					$messages = [
-						'type' => 'template', // 訊息類型 (模板)
-                				'altText' => 'ลงทะเบียน', // 替代文字
-                				'template' => array(
-                    						'type' => 'buttons', // 類型 (按鈕)
-		                				'thumbnailImageUrl' => 'https://okplus.co.th/Bot/Images/ImgButtonTemplate.png', // 圖片網址 <不一定需要>
-                 						'title' => 'บ.โอเคพลัส จำกัด', // 標題 <不一定需要>
-		                				'text' => 'ยินดีต้อนรับเข้าสู่ระบบ', // 文字
-                						'actions' => array(
-			                      					//  array(
-                            							//	'type' => 'postback', // 類型 (回傳)
-				                 				//       'label' => 'Postback example', // 標籤 1
-				                   				//     'data' => 'action=buy&itemid=123' // 資料
-                        			  				//    ),
-			                       					// array(
-                            							//	'type' => 'message', // 類型 (訊息)
-				                 				//       'label' => 'Message example', // 標籤 2
-				                   				//     'text' => 'Message example' // 用戶發送文字
-				                 				//     ),
-			                        				   array(
-                        				 				'type' => 'uri', // 類型 (連結)
-				                         				'label' => 'ลงทะเบียน', // 標籤 3
-				                         				'uri' => 'http://okplus.ddns.net/okplus/OKMO/Bot.aspx?u='.$text // 連結網址
-				                       				         )
-			                       					   )
-		                					)
-					
+						
 						];	
 						// end message
    					 break;
   					 case "ใบเสร็จ":
    							
-					//$paymentDetails = file_get_contents('http://okplus.ddns.net/okplus/bot/getPaymentList.aspx');
-					$paymentDetails = "1,430:Big C:14 เมษายน 2563:ชำระค่างวดรถจักรยานยนต์:ราณี สายใจ:8กร 2513:62RC-06200:62/0147:621478";
-					$str_arr = explode (":", $paymentDetails);  
 					
-					$amount=$str_arr[0];
-					$channel = $str_arr[1];
-					$dt = $str_arr[2];
-					$detail = $str_arr[3];
-					$name = $str_arr[4];
-					$plate = $str_arr[5];
-					$receiptId = $str_arr[6];
-					$contractId = $str_arr[7];
-					$reference = $str_arr[8];
 					
 					
 					
@@ -160,183 +98,6 @@ if (!is_null($events['events'])) {
 				
 				// Build message to reply back
 				$messages = [
-					
-					
-					
-						 "type" => "flex",
-    "altText" => "ใบเสร็จรับเงิน",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "ใบเสร็จรับเงิน",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
-          ],
-          [
-            "type" => "text",
-            "text" => "฿".$amount,
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => $name,
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => $dt,
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => $detail,
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
-          ]
-        ]
-      ],
-      "body" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "separator",
-            "color" => "#C3C3C3"
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "เลขที่ใบเสร็จรับเงิน",
-                "align" => "start",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => $receiptId,
-                "align" => "end",
-                "color" => "#000000"
-              ]
-            ]
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "สัญญาเลขที่",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => $contractId,
-                "align" => "end"
-              ]
-            ]
-          ],
-		
-		
-		 [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "เลขที่อ้างอิง",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => $reference,
-                "align" => "end"
-              ]
-            ]
-          ],
-		 [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "ชำระทาง",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => $channel,
-                "align" => "end"
-              ]
-            ]
-          ],
-		
-		 [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "ทะเบียนรถ",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => $plate,
-                "align" => "end"
-              ]
-            ]
-          ],
-		
-		
-		
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
-          ]
-        ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "จ่ายตรงวัน ลดงวดละ 100 บาท",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://www.okplus.co.th"
-            ]
-          ]
-        ]
-      ]
-    ]
-					
 					
 					
 					
@@ -353,7 +114,7 @@ if (!is_null($events['events'])) {
   				         default:
 							    $messages = [
 										'type' => 'text',
-										'text' => 'Line นี้เป็นระบบอัตโนมัติ'."\n".'หากต้องการติดต่อพนักงาน โปรดติดต่อที่ https://lin.ee/6D052q8'	
+										'text' => 'กรุณารอสักครู่นะค่ะ'	
 									];	
 				// end case				
 				}
