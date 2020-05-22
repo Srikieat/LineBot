9b78e5a8b31c51c37ec3d8ec752986537cc4e82c
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-22 10:52:17 +0700

    Update webhook_okplus_motor.php

--- webhook_okplus_motor.php
+++ webhook_okplus_motor.php
@@ -236,7 +236,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
+  
 			echo "Reply : " .$result . "\r\n";
 		}
 	}
