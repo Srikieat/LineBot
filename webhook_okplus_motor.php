a2ff3ed730913f4fba1f6138b941b3fe242623cc
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-22 10:58:25 +0700

    Update webhook_okplus_motor.php

--- webhook_okplus_motor.php
+++ webhook_okplus_motor.php
@@ -202,8 +202,8 @@ if (!is_null($events['events'])) {
 							'type' => 'buttons', // 類型 (按鈕)
 						'thumbnailImageUrl' => 'https://okplus.co.th/images/bike/pcx1.png', // 圖片網址 <不一定需要>
 						 'title' => 'Scoopy', // 標題 <不一定需要>
-						'text' => 'รุ่นนี้ฟรีดาวน์'."\n".'ออกรถ 0 บ. ไม่ต้องใช้คนค้ำ'."\n".'ฟรีประกันรถหาย + พรบ + จดทะบียน', // 文字
-						'actions' => array(
+						'text' => 'รุ่นนี้ฟรีดาวน์'."\n".'ออกรถ 0 บาท ไม่ต้องใช้คนค้ำ'."\n".'ฟรีประกันรถหาย + พรบ + จดทะบียน', // 文字
+						'actions' => array(    
 											 array(
 										  'type' => 'message', // 類型 (連結)
 										  'label' => 'สนใจออกรถออนไลน์', // 標籤 3
