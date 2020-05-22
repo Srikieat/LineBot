9d1a83b08977215f130b0c881cdefd48e861c892
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-22 11:33:04 +0700

    Update botPushOkplusMotor.php

--- botPushOkplusMotor.php
+++ botPushOkplusMotor.php
@@ -1,1 +1,30 @@
+<?php
+
+
+
+require "vendor/autoload.php";
+
+$access_token = '0jFIiIq0JnX9WLpNo+ZMNnVKOSP3IYtDwwqLNSwnR3PyIqo+pTSIdJyY0fLkxQEBSGB7h1OA/ZlRTeHiYeb6v/B7Xnla6B2RO0oIjXfuLFKLKp5kwGc1ZwyR/Ye2KAAnD+fXr3MR7/eCN6ilzs6CQAdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = 'df1d8d8ef1b407d1c31c7b1aac6e8027';
+
+
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$msg = $_GET['m'];
+$pushID = $_GET['u'];
+  //'U44e90a4578cb725ccc9ed09d2cdc18e9';
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+
+
+
+
+
 
