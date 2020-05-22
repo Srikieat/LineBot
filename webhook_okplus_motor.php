030220886aa0a1011ec024032f3868370ba09bba
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-05-22 14:22:04 +0700

    Update webhook_okplus_motor.php

--- webhook_okplus_motor.php
+++ webhook_okplus_motor.php
@@ -425,7 +425,7 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
+ 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
