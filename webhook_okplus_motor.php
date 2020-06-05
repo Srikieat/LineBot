e1e4f584a4ff0eb5b53d8ae89715d07dec189011
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-06-05 12:56:18 +0700

    Update webhook_okplus_motor.php

--- webhook_okplus_motor.php
+++ webhook_okplus_motor.php
@@ -334,14 +334,14 @@ if (!is_null($events['events'])) {
 				if (checkSendMessage($dataCheckHave,$sendMessage) == 1)
 				{
 					$sendMessage = "BlackList";
-					$state = "0031";
+
 				}
 				else
 				{
 					$isMoreMessage =1;
 					$skipAnswer  = 1;
 					$isNeedHelp = 0;
-					$state = "0041";
+					
 				}
 			}
 			
