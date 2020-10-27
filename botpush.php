31f1875e9b353032d93d45d85b0ba5b658f6498f
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-10-27 09:33:22 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -14,9 +14,10 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$msg = $_GET['m'];
-$pushID = $_GET['u'];
-  //'U44e90a4578cb725ccc9ed09d2cdc18e9';
+//$msg = $_GET['m'];
+//$pushID = $_GET['u'];
+$pushID = 'U44e90a4578cb725ccc9ed09d2cdc18e9';
+$msg = 'สวัสดี ศรีเกียรติ';
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
