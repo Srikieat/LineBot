2d4508ffd09954a9a950aab381a322862c7f23a2
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-11-02 11:45:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -138,7 +138,7 @@ if (!is_null($events['events'])) {
 				                 				//     ),
 			                        				   array(
                         				 				'type' => 'uri', // 類型 (連結)
-				                         				'label' => 'นัดชำระค่างวด', // 標籤 3
+				                         				'label' => 'ดำเนินการ', // 標籤 3
 				                         				'uri' => 'http://okplus.ddns.net/okplus/OKMO/BotAppint.aspx?u='.$text // 連結網址
 				                       				         )
 			                       					   )
