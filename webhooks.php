63ca5afee240f8667bac2b873685382b8215f9bc
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-11-17 14:52:22 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -116,7 +116,49 @@ if (!is_null($events['events'])) {
 			switch ($sendMessage) {
 				case "นัดชำระค่างวด":
 					
-					// start message
+					// check register or not
+					//$isRegister = file_get_contents('http://okplus.ddns.net/okplus/bot/CheckRegister.aspx?u='.$id);
+					//$str_arr = explode (":", $paymentDetails);  
+					
+					//$isRegister=$str_arr[0];
+					$isRegister = "-1";
+					
+					if ($isRegister === "-1")
+					{
+						
+						$messages = [
+						'type' => 'template', // 訊息類型 (模板)
+                				'altText' => 'ลงทะเบียน', // 替代文字
+                				'template' => array(
+                    						'type' => 'buttons', // 類型 (按鈕)
+		                				'thumbnailImageUrl' => 'https://okplus.co.th/Bot/Images/ImgButtonTemplate.png', // 圖片網址 <不一定需要>
+                 						'title' => 'บ.โอเคพลัส จำกัด', // 標題 <不一定需要>
+		                				'text' => 'ท่านยังไม่ได้ลงทะเบียนในระบบ กรุณาลงทะเบียน', // 文字
+                						'actions' => array(
+			                      					//  array(
+                            							//	'type' => 'postback', // 類型 (回傳)
+				                 				//       'label' => 'Postback example', // 標籤 1
+				                   				//     'data' => 'action=buy&itemid=123' // 資料
+                        			  				//    ),
+			                       					// array(
+                            							//	'type' => 'message', // 類型 (訊息)
+				                 				//       'label' => 'Message example', // 標籤 2
+				                   				//     'text' => 'Message example' // 用戶發送文字
+				                 				//     ),
+			                        				   array(
+                        				 				'type' => 'uri', // 類型 (連結)
+				                         				'label' => 'ลงทะเบียน', // 標籤 3
+				                         				'uri' => 'http://okplus.ddns.net/okplus/OKMO/Bot.aspx?u='.$text // 連結網址
+				                       				         )
+			                       					   )
+		                					)
+					
+						];	
+						
+					}
+					if ($isRegister === "1")
+					{
+						// start message
 					$messages = [
 						'type' => 'template', // 訊息類型 (模板)
                 				'altText' => 'นัดชำระค่างวด', // 替代文字
@@ -146,6 +188,8 @@ if (!is_null($events['events'])) {
 					
 						];	
 						// end message
+					}
+					
 					
 					break;
   				case "ลงทะเบียน":
