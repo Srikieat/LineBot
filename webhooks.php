0754506f89cdab6d13e5bcf03927d29dfcb53d74
Srikieat <42906808+Srikieat@users.noreply.github.com>
2020-11-17 14:56:55 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -156,25 +156,10 @@ if (!is_null($events['events'])) {
 						];	
 						
 					}
-					
 					if ($isRegister === "1")
 					{
-						
-						// check already appointment
-						//$serverData = file_get_contents('http://okplus.ddns.net/okplus/bot/CheckRegister.aspx?u='.$id);
-						//$serverData = "1;srikieat;12/45263;16April2020";
-						//$str_arr = explode (":", $serverData); 
-						
-						//$isAppoint = $str_arr[0];
-						//$name=$str_arr[1];
-						//$contractId = $str_arr[2];
-						//$dt = $str_arr[3];
-						
-						//if ($isAppoint == "0")
-						//{
-							// still not appoint : can do appointment
-							// start message
-						$messages = [
+						// start message
+					$messages = [
 						'type' => 'template', // 訊息類型 (模板)
                 				'altText' => 'นัดชำระค่างวด', // 替代文字
                 				'template' => array(
@@ -201,22 +186,8 @@ if (!is_null($events['events'])) {
 			                       					   )
 		                					)
 					
-									];	
-									// end message
-							
-						}
-							
-						//if ($isAppoint == "1")
-						//{
-							
-							// $messages = 	[
-								//				'type' => 'text',
-								 //				'text' => 'test'
-								 				//'text' => 'ชื่อ : '	.$name .."\n".'เลขที่สัญญา่ : '$contractId.."\n".'นัดวันที่ : '  $dt
-									//		];	
-						//}
-						
-						
+						];	
+						// end message
 					}
 					
 					
