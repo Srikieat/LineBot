c8baa94f2b30790fb5fd83b0535ec187e7795cbe
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-01-08 14:10:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -105,40 +105,46 @@ if (!is_null($events['events'])) {
 		
 		// detect image
 		//
-		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
-			
-				// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			
-			// reply message
-			 $messages = [
-										'type' => 'text',
-										'text' => 'Line นี้เป็นระบบอัตโนมัติ'."\n"."\n".'หากต้องการส่งสลิปการชำระค่างวด โปรดส่งสลิปมาที่ Line ด้านล่างนี้ค่ะ  https://lin.ee/6D052q8'."\n"."\n".'ขอบคุณค่ะ'
-									];	
+		if ($event['type'] == 'message' && $event['message']['type'] == 'image') 
+		{
 			
 			
 			
-				// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+			
+			
+			
+			
+			 if ($array['events'][0]['message']['type'] == 'video') 
+			 	{
+        			$F_TYPE = "mp4";
+    			}
+			
+    		if ($array['events'][0]['message']['type'] == 'image') 
+			{
+        			$F_TYPE = "png";
+			}
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+    		$message_id = $array['events'][0]['message']['id'];
 
-			echo "Reply : " .$result . "\r\n";
+    		$response = $bot->getMessageContent($message_id);
+    		$date_file = date("Y-m-d-H-i-s");
+
+    		if ($response->isSucceeded()) {
+
+        		$file_save_temp = "uploadImages/$date_file.$F_TYPE";
+        		$fs = fopen($file_save_temp, "w");
+        		fwrite($fs, $response->getRawBody());
+        		fclose($fs);
+
+        		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('save  data แล้วคร้าา' . "\r\n $response_data");
+        		$response = $bot->pushMessage($to, $textMessageBuilder);
+			}
+			
+			
+			
+			
+				
 		}
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
