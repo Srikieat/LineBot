48e710742f52226a197801f3432e4c0fbaa04d1b
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-01-09 09:38:23 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -130,11 +130,15 @@ if (!is_null($events['events'])) {
 					file_put_contents($fileFullSavePath,$dataBinary);
 				
 				
-
-					//$msg = $_GET['m'];
-					//$pushID = $_GET['u'];
+				
+					 $accessToken = "0jFIiIq0JnX9WLpNo+ZMNnVKOSP3IYtDwwqLNSwnR3PyIqo+pTSIdJyY0fLkxQEBSGB7h1OA/ZlRTeHiYeb6v/B7Xnla6B2RO0oIjXfuLFKLKp5kwGc1ZwyR/Ye2KAAnD+fXr3MR7/eCN6ilzs6CQAdB04t89/1O/w1cDnyilFU=";
+					//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+   					$arrayHeader = array();
+   					$arrayHeader[] = "Content-Type: application/json";
+   					$arrayHeader[] = "Authorization: Bearer {$accessToken}";
 					$pushID = 'U44e90a4578cb725ccc9ed09d2cdc18e9';
-					$msg = 'สวัสดี ศรีเกียรติ';
+					
+				
 				
 					$messages = [
 						'type' => 'template', // 訊息類型 (模板)
@@ -164,11 +168,28 @@ if (!is_null($events['events'])) {
 		                					)
 					
 						];	
+					$data = [
+						'to' => $pushID,
+						'messages' => [$messages],
+					];
+					$post = $data;
+
+					$strUrl = "https://api.line.me/v2/bot/message/push";
+      				$ch = curl_init();
+      				curl_setopt($ch, CURLOPT_URL,$strUrl);
+      				curl_setopt($ch, CURLOPT_HEADER, false);
+      				curl_setopt($ch, CURLOPT_POST, true);
+      				curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      				curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+      				curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      				$result = curl_exec($ch);
+      				curl_close ($ch);
+					
+				
+					
 				
 				
-				
-					$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messages);
-					$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 				
 			}
