4ea3c923117a6718a9172837318f629318695e2c
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-01-09 09:49:06 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -141,29 +141,32 @@ if (!is_null($events['events'])) {
 				
 				
 					$messages = [
-						 
-          type: "template",
-          altText: "This is an image carousel template",
-          template: {
-            type: "image_carousel",
-            columns: [
-              {
-                imageUrl: "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png",
-                action: {
-                  type: "message",
-                  label: "Brown",
-                  text: "Brown was selected"
-                }
-              },
-              {
-                imageUrl: "https://vignette.wikia.nocookie.net/line/images/1/10/2015-cony.png",
-                action: {
-                  type: "uri",
-                  label: "Cony",
-                  uri: "https://developers.line.biz"
-                }
-			  }
-				]
+						 'type' => 'template', // 訊息類型 (模板)
+                				'altText' => 'ลงทะเบียน', // 替代文字
+                				'template' => array(
+                    						'type' => 'buttons', // 類型 (按鈕)
+		                				'thumbnailImageUrl' => 'https://okplus.co.th/Bot/Images/ImgButtonTemplate.png', // 圖片網址 <不一定需要>
+                 						'title' => 'บ.โอเคพลัส จำกัด', // 標題 <不一定需要>
+		                				'text' => 'ยินดีต้อนรับเข้าสู่ระบบ', // 文字
+                						'actions' => array(
+			                      					//  array(
+                            							//	'type' => 'postback', // 類型 (回傳)
+				                 				//       'label' => 'Postback example', // 標籤 1
+				                   				//     'data' => 'action=buy&itemid=123' // 資料
+                        			  				//    ),
+			                       					// array(
+                            							//	'type' => 'message', // 類型 (訊息)
+				                 				//       'label' => 'Message example', // 標籤 2
+				                   				//     'text' => 'Message example' // 用戶發送文字
+				                 				//     ),
+			                        				   array(
+                        				 				'type' => 'uri', // 類型 (連結)
+				                         				'label' => 'ลงทะเบียน', // 標籤 3
+				                         				'uri' => 'http://okplus.ddns.net/okplus/OKMO/Bot.aspx?u='.$text // 連結網址
+				                       				         )
+			                       					   )
+		                					)
+    
 						];	
 					$data = [
 						'to' => $pushID,
