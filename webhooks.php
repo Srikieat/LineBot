d889488ed176d50f4eb49c8b5795ac6599c3660b
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-03-01 14:48:16 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -140,6 +140,9 @@ if (!is_null($events['events'])) {
 				$fileFullSavePath = 'uploadImages/'.$date_file.'.jpg';
 				
 				file_put_contents($fileFullSavePath,$results['response']);
+				
+				$urlImage = 'https://okplusbot.herokuapp.com/'.$fileFullSavePath;
+				
 				$aaa = '2';
     		}
 
@@ -173,7 +176,7 @@ if (!is_null($events['events'])) {
 					
 										'type' => 'text',
 										//'text' => 'Line นี้เป็นระบบอัตโนมัติ'."\n"."\n".'หากต้องการส่งสลิปการชำระค่างวด โปรดส่งสลิปมาที่ Line ด้านล่างนี้ค่ะ  https://lin.ee/6D052q8'."\n"."\n".'ขอบคุณค่ะ'
-				 						'text' => $fileFullSavePath
+				 						'text' => $urlImage
 				];
 				
 					//$messages = [
