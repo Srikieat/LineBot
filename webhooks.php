e8147af22e676e611f09e518893764fd077a9854
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-03-01 14:41:04 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -127,7 +127,7 @@ if (!is_null($events['events'])) {
 			
 			$LINEDatas['token'] = $access_token;
 			
-			//$LINEDatas['messageId'] = $deCode['events'][0]['message']['id'];
+			$LINEDatas['messageId'] = $deCode['events'][0]['message']['id'];
 
 			//if ($response->isSucceeded()) {
 				$aaa = '2';
@@ -1276,3 +1276,38 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "test";
+
+
+function getContent($datas)
+  {
+    $datasReturn = [];
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.line.me/v2/bot/message/".$datas['messageId']."/content",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_POSTFIELDS => "",
+      CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer ".$datas['token'],
+        "cache-control: no-cache"
+      ),
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+
+    if($err){
+      $datasReturn['result'] = 'E';
+      $datasReturn['message'] = $err;
+    }else{
+      $datasReturn['result'] = 'S';
+      $datasReturn['message'] = 'Success';
+      $datasReturn['response'] = $response;
+    }
+
+    return $datasReturn;
+  }
