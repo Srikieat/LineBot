d05d40beb9b9b2e9baf78151e14c48c63d76b84b
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-09-14 11:10:51 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,26 @@
 <?php
 
-$string = "1,430:ชำระทาง Big C:14 เมษายน"; 
-$str_arr = explode (":", $string);  
-print_r($str_arr); 
-print($str_arr[0]); 
+$ftp_server="119.59.120.23";
+ $ftp_user_name="okplusc1";
+ $ftp_user_pass="2A3w7tFm7j";
+ $file = "uploadImages/image.png";//tobe uploaded
+ $remote_file = "public_html/RegCopy/image.png";
+
+ // set up basic connection
+ $conn_id = ftp_connect($ftp_server);
+
+ // login with username and password
+ $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+
+ // upload a file
+ if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
+    echo "successfully uploaded $file\n";
+    exit;
+ } else {
+    echo "There was a problem while uploading $file\n";
+    exit;
+    }
+ // close the connection
+ ftp_close($conn_id);
 
 ?>
