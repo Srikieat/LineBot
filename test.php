7c06a7a11ab09af4318c193205e4559150631230
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-09-14 11:39:02 +0700

    Update test.php

--- test.php
+++ test.php
@@ -40,26 +40,26 @@ $ftp_user_name = "okplusc1"; // Username
 $ftp_user_pass = '2A3w7tFm7j'; // Password
 $destination_file = "/public_html/RegCopy/image.png"; //where you want to throw the file on the webserver (relative to your login dir)
 
-$conn_id = ftp_connect($ftp_server) or die("<span style='color:#FF0000'><h2>Couldn't connect to $ftp_server</h2></span>");        // set up basic connection
+//$conn_id = ftp_connect($ftp_server) or die("<span style='color:#FF0000'><h2>Couldn't connect to $ftp_server</h2></span>");        // set up basic connection
 
-$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass) or die("<span style='color:#FF0000'><h2>You do not have access to this ftp server!</h2></span>");   // login with username and password, or give invalid user message
+//$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass) or die("<span style='color:#FF0000'><h2>You do not have access to this ftp server!</h2></span>");   // login with username and password, or give invalid user message
 
-if ((!$conn_id) || (!$login_result)) {  // check connection
+//if ((!$conn_id) || (!$login_result)) {  // check connection
     // wont ever hit this, b/c of the die call on ftp_login
-    echo "<span style='color:#FF0000'><h2>FTP connection has failed! <br />";
-    echo "Attempted to connect to $ftp_server for user $ftp_user_name</h2></span>";
-    exit;
-} else {
+  //  echo "<span style='color:#FF0000'><h2>FTP connection has failed! <br />";
+    //echo "Attempted to connect to $ftp_server for user $ftp_user_name</h2></span>";
+    //exit;
+//} else {
     //echo "Connected to $ftp_server, for user $ftp_user_name <br />";
-}
+//}
 
-$upload = ftp_put($conn_id, $destination_file.$name, $filename, FTP_BINARY);  // upload the file
-if (!$upload) {  // check upload status
-    echo "<span style='color:#FF0000'><h2>FTP upload of $filename has failed!</h2></span> <br />";
-} else {
-    echo "<span style='color:#339900'><h2>Uploading $name Completed Successfully!</h2></span><br /><br />";
-}
-ftp_close($conn_id); // close the FTP stream    
+//$upload = ftp_put($conn_id, $destination_file.$name, $filename, FTP_BINARY);  // upload the file
+//if (!$upload) {  // check upload status
+  //  echo "<span style='color:#FF0000'><h2>FTP upload of $filename has failed!</h2></span> <br />";
+//} else {
+  //  echo "<span style='color:#339900'><h2>Uploading $name Completed Successfully!</h2></span><br /><br />";
+//}
+//ftp_close($conn_id); // close the FTP stream    
 
 
 ?>
