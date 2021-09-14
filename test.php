b8aadb7e1878075cb4fb133f36c27bdb1bf5776b
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-09-14 11:30:38 +0700

    Update test.php

--- test.php
+++ test.php
@@ -10,7 +10,7 @@ $ftp_server="119.59.120.23";
 echo '1';
 
 // set up basic connection
- //$conn_id = ftp_connect($ftp_server);
+ $conn_id = ftp_connect($ftp_server);
 
  // login with username and password
  //$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
