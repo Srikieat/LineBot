0a22c700d8db0f5c351ec153e862647f164149bc
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-09-14 09:40:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -240,7 +240,7 @@ if (!is_null($events['events'])) {
 			// reply message
 			 $messages = [
 										'type' => 'text',
-				 						'text' => $urlImage;
+				 						'text' => $urlImage
 										//'text' => 'Line นี้เป็นระบบอัตโนมัติ'."\n"."\n".'หากต้องการส่งสลิปการชำระค่างวด โปรดส่งสลิปมาที่ Line ด้านล่างนี้ค่ะ  https://lin.ee/6D052q8'."\n"."\n".'ขอบคุณค่ะ'
 				 						//'text' => 'ขอบคุณค่ะ'
 									];	
