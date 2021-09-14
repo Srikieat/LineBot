765c38f277ceaee287bdc378c11084fbecad17dc
Srikieat <42906808+Srikieat@users.noreply.github.com>
2021-09-14 09:17:32 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -155,11 +155,11 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $data =curl_exec($ch);
 
-curl_close($ch);
-$fp = ‘uploadImages/’.$msg_id.’.png’;
+//curl_close($ch);
+//$fp = ‘uploadImages/’.$msg_id.’.png’;
 //$url_img=”http://103.40.151.6/line_bot_gts_issue/”.$fp;
-file_put_contents( $fp, $data );
-						$content = $fp;
+//file_put_contents( $fp, $data );
+//						$content = $fp;
 			
 			//$date_file = date("Y-m-d-H-i-s");
 		//	$date_file = uniqid();
