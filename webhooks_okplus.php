732d4e2a109d7c516d2cf492b924a37327bfc4ca
Srikieat <42906808+Srikieat@users.noreply.github.com>
2025-04-18 09:04:33 +0700

    Update webhooks_okplus.php

--- webhooks_okplus.php
+++ webhooks_okplus.php
@@ -144,17 +144,7 @@ if (!is_null($events['events'])) {
 			 	];	
 			
 			
-			 $messages = [
-                'type' => 'text',
-                'text' => $userName
-            		];	
-			
-			$url = 'https://api.line.me/v2/bot/message/reply';
 		
-				$data = [
-			 	'replyToken' => $replyToken,
-				'messages' => [$messages]
-			 	];	
 			
 			
 			
