a3159acc52f0ff9f7aa66f1a6b8e48d1df1a26c7
Srikieat <42906808+Srikieat@users.noreply.github.com>
2025-04-18 10:45:49 +0700

    Update webhooks_okplus.php

--- webhooks_okplus.php
+++ webhooks_okplus.php
@@ -117,8 +117,13 @@ if (!is_null($events['events'])) {
 			
 			$paymentDetails = file_get_contents($url);
 			
-			curl_init($url);
-			
+			$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_exec($ch);
 			$messages = [
                 'type' => 'text',
                'text' => $url
