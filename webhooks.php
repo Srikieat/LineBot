0b3f5a48a49d2e6b546b8d5fc5f3b76e4516f76b
Srikieat <42906808+Srikieat@users.noreply.github.com>
2025-04-20 02:39:13 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1401,7 +1401,10 @@ if (!is_null($events['events'])) {
    					 break;
   					 case "ใบเสร็จ":
    							
-					$paymentDetails = file_get_contents('http://okplus.thddns.net:9330/okplus/bot/getPaymentList.aspx');
+					//$paymentDetails = file_get_contents('http://okplus.thddns.net:9330/okplus/bot/getPaymentList.aspx');
+					$url = 'http://okplus.thddns.net:9330/okplus/bot/getPaymentList.aspx';
+					$ch = curl_init($url);
+					$paymentDetails = curl_exec($ch);
 					//$paymentDetails = "1,430:Big C:14 เมษายน 2563:ชำระค่างวดรถจักรยานยนต์:ราณี สายใจ:8กร 2513:62RC-06200:62/0147:621478";
 					$str_arr = explode (":", $paymentDetails);  
 					
