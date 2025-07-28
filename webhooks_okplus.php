cac62af4e49386989cf1f7122cd42ff0fdc9967c
Srikieat <42906808+Srikieat@users.noreply.github.com>
2025-07-28 09:35:59 +0700

    Update webhooks_okplus.php

--- webhooks_okplus.php
+++ webhooks_okplus.php
@@ -190,7 +190,7 @@ if (!is_null($events['events'])) {
 			 	];	
 			
 			
-			$url = 'http:///okplus.thddns.net:9330/okplus/bot/okplusMotorFollow.aspx?u='.$id.'&n='.$userName;
+			$url = 'http:///okplus.thddns.net:9330/okplus/bot/okplusMotorFollow.aspx?s=okplus&u='.$id.'&n='.$userName;
 			$ch = curl_init($url);
 			$result = curl_exec($ch);
 			
